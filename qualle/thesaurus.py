bd6356c8c826990afa11e9dbba3b6505c4b065a0
Christopher Bartz <c.bartz@zbw.eu>
2021-03-30 20:21:31 +0200

    Transformer which calculates count of labels per subthesauri for a given rdf graph

---
+++ qualle/thesaurus.py
@@ -0,0 +1,80 @@
+#  Copyright (c) 2021 ZBW  – Leibniz Information Centre for Economics
+#
+#  This file is part of qualle.
+#
+#  qualle is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  qualle is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with qualle.  If not, see <http://www.gnu.org/licenses/>.
+import logging
+from collections import defaultdict
+from typing import List, Set
+
+import numpy as np
+from rdflib import URIRef, Graph
+from rdflib.namespace import SKOS, RDF
+from sklearn.base import TransformerMixin, BaseEstimator
+from sklearn.utils.validation import check_is_fitted
+
+from qualle.models import Concepts
+
+
+class LabelCountForSubthesauriTransformer(BaseEstimator, TransformerMixin):
+    """Compute count of labels per Subthesauri for a given RDF Graph."""
+    def __init__(
+            self,
+            graph: Graph,
+            subthesaurus_type_uri: URIRef,
+            concept_type_uri: URIRef,
+            subthesauri: List[URIRef],
+
+    ):
+        self.graph = graph
+        self.subthesaurus_type_uri = subthesaurus_type_uri
+        self.concept_type_uri = concept_type_uri
+        self.subthesauri = subthesauri
+
+    def fit(self, X=None, y=None):
+        self.mapping_ = defaultdict(lambda: [False] * len(self.subthesauri))
+        for idx, s in enumerate(self.subthesauri):
+            concepts = self._get_concepts_for_thesaurus(s)
+            for c in concepts:
+                self.mapping_[c][idx] = True
+        return self
+
+    def transform(self, X: List[Concepts]) -> np.array:
+        """Transform rows of concepts to 2-dimensional count array
+
+        Each row in the result array  contains in each column the total
+        amount of labels per subthesauri. The subthesauri are given
+        in the same order as in the list passed to the constructor.
+        """
+        check_is_fitted(self)
+        count_matrix = np.zeros((len(X), len(self.subthesauri)))
+        for row_idx, row in enumerate(X):
+            for concept in row:
+                for j, is_in_subthesauri in enumerate(self.mapping_[concept]):
+                    count_matrix[row_idx, j] = count_matrix[row_idx, j] + int(
+                        is_in_subthesauri)
+        return count_matrix
+
+    def _get_concepts_for_thesaurus(self, thesaurus: URIRef) -> Set:
+        concepts = set()
+        for x in self.graph[thesaurus:SKOS.narrower]:
+            if (x, RDF.type, self.subthesaurus_type_uri) in self.graph:
+                concepts_from_subthesaurus = self._get_concepts_for_thesaurus(
+                    x)
+                concepts = concepts.union(concepts_from_subthesaurus)
+            elif (x, RDF.type, self.concept_type_uri) in self.graph:
+                concepts.add(x)
+            else:
+                logging.warning('unknown narrower type %s', str(x))
+        return concepts
