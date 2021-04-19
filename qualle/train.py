3fa4dacf7f720adad8c2f015d09f59a0c4528acc
Christopher Bartz <c.bartz@zbw.eu>
2021-04-19 17:34:29 +0200

    Pass Features to use to Trainer

--- qualle/train.py
+++ qualle/train.py
@@ -14,11 +14,23 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with qualle.  If not, see <http://www.gnu.org/licenses/>.
+from typing import Set, Type, List
 
+from sklearn.base import RegressorMixin
 
+from qualle.features.base import Features
+from qualle.features.combined import CombinedFeaturesData, CombinedFeatures
+from qualle.features.confidence import ConfidenceFeatures
 from qualle.features.label_calibration.base import AbstractLabelCalibrator
+from qualle.features.label_calibration.simple_label_calibration import \
+    SimpleLabelCalibrationFeatures
+from qualle.features.label_calibration.thesauri_label_calibration import \
+    ThesauriLabelCalibrationFeatures
+from qualle.models import PredictData, LabelCalibrationData
 from qualle.pipeline import QualityEstimationPipeline
 from qualle.quality_estimation import RecallPredictor
+
+FeaturesTypes = Set[Type[Features]]
 
 
 class Trainer:
@@ -26,15 +38,21 @@ class Trainer:
     def __init__(
             self, train_data,
             label_calibrator: AbstractLabelCalibrator,
-            recall_predictor: RecallPredictor,
+            recall_predictor_regressor: RegressorMixin,
+            features: List[Features],
             should_debug=False
     ):
+        combined_features = CombinedFeatures(features)
+        rp = RecallPredictor(
+            regressor=recall_predictor_regressor, features=combined_features
+        )
         self._qe_p = QualityEstimationPipeline(
-            recall_predictor=recall_predictor,
+            recall_predictor=rp,
             label_calibrator=label_calibrator,
             should_debug=should_debug,
-            # Can't use lambda because of pickle
-            features_data_mapper=features_data_mapper
+            features_data_mapper=FeaturesDataMapper(
+                set(f.__class__ for f in features)
+            )
         )
         self._train_data = train_data
 
@@ -44,5 +62,20 @@ class Trainer:
         return self._qe_p
 
 
-def features_data_mapper(_, label_calibration_data):
-    return label_calibration_data
+class FeaturesDataMapper:
+
+    def __init__(self, features_types: FeaturesTypes):
+        self._features_types = features_types
+
+    def __call__(self, p_data: PredictData, l_data: LabelCalibrationData)\
+            -> CombinedFeaturesData:
+        features_data = dict()
+        for ftype in self._features_types:
+            if ftype == ConfidenceFeatures:
+                features_data[ConfidenceFeatures] = p_data.scores
+            if ftype in (
+                    SimpleLabelCalibrationFeatures,
+                    ThesauriLabelCalibrationFeatures
+            ):
+                features_data[ftype] = l_data
+        return features_data
--- tests/test_train.py
+++ tests/test_train.py
@@ -14,24 +14,60 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with qualle.  If not, see <http://www.gnu.org/licenses/>.
+
+import numpy as np
+import pytest
 from sklearn.ensemble import ExtraTreesRegressor
 
+from qualle.features.confidence import ConfidenceFeatures
 from qualle.features.label_calibration.simple_label_calibration import \
     SimpleLabelCalibrator, SimpleLabelCalibrationFeatures
-from qualle.quality_estimation import RecallPredictor
-from qualle.train import Trainer
+from qualle.models import LabelCalibrationData
+from qualle.train import Trainer, FeaturesDataMapper
+
+
+@pytest.fixture
+def l_data(train_data):
+    p_data = train_data.predict_data
+    return LabelCalibrationData(
+        predicted_no_of_labels=np.array([0] * 5),
+        predicted_labels=p_data.predicted_labels
+    )
 
 
 def test_train_trains_qe_pipeline(train_data, mocker):
     t = Trainer(
         train_data=train_data,
         label_calibrator=SimpleLabelCalibrator(ExtraTreesRegressor()),
-        recall_predictor=RecallPredictor(
-            regressor=ExtraTreesRegressor(),
-            features=SimpleLabelCalibrationFeatures()
-        )
+        recall_predictor_regressor=ExtraTreesRegressor(),
+        features=[SimpleLabelCalibrationFeatures()]
     )
     spy = mocker.spy(t._qe_p, 'train')
     t.train()
 
     spy.assert_called_once()
+
+
+def test_features_data_mapper_with_lc(train_data, l_data):
+    p_data = train_data.predict_data
+    mapper = FeaturesDataMapper(set([
+        SimpleLabelCalibrationFeatures, ConfidenceFeatures])
+    )
+    assert mapper(p_data, l_data) == {
+        SimpleLabelCalibrationFeatures: l_data,
+        ConfidenceFeatures: p_data.scores
+    }
+
+
+def test_features_data_mapper_without_lc(train_data, l_data):
+    p_data = train_data.predict_data
+    mapper = FeaturesDataMapper(set([ConfidenceFeatures]))
+    assert mapper(p_data, l_data) == {
+        ConfidenceFeatures: p_data.scores
+    }
+
+
+def test_features_data_mapper_without_features(train_data, l_data):
+    p_data = train_data.predict_data
+    mapper = FeaturesDataMapper(set())
+    assert mapper(p_data, l_data) == {}
